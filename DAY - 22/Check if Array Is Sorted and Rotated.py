edd987028d6c6a7dd12b440f3624a2ced334edc6
GopikaSri <gopikasri2k7@gmail.com>
2025-05-22 21:12:27 +0530

    Create Check if Array Is Sorted and Rotated.py

---
+++ DAY - 22/Check if Array Is Sorted and Rotated.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def check(self, nums):
+        count = 0
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                count += 1
+
+        if nums[-1] > nums[0]:
+            count += 1
+
+        if count <= 1:
+            return True
+        
+        else:
+            return False
+        
