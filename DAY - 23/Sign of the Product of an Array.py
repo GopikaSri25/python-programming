c0f19ce30686097c76182ace33de624a748082b1
GopikaSri <gopikasri2k7@gmail.com>
2025-05-22 21:40:04 +0530

    Create Sign of the Product of an Array.py

---
+++ DAY - 23/Sign of the Product of an Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def arraySign(self, nums):
+        product = 1
+        for i in nums:
+            product *= i
+        if product > 0:
+            return 1
+        elif product < 0:
+            return -1
+        else:
+            return 0
+        
